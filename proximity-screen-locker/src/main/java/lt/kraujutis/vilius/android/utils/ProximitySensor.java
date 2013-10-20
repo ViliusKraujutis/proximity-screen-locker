b3eb05404e6cb60d233078c9a148afb67c4bc483
Vilius Kraujutis <viliusk@gmail.com>
2013-10-20 20:41:34 +0300

    Check if Proximity sensor exists
    There were few crashes on devices without Proximity sensor.

--- proximity-screen-locker/src/main/java/lt/kraujutis/vilius/android/utils/ProximitySensor.java
+++ proximity-screen-locker/src/main/java/lt/kraujutis/vilius/android/utils/ProximitySensor.java
@@ -7,50 +7,54 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import lt.kraujutis.vilius.android.screenlocker.R;
 
 public class ProximitySensor {
 
 	private static final String TAG = ProximitySensor.class.getCanonicalName();
-	private static ProximitySensor mProximitySensor;
-	public float value;
+    private static ProximitySensor mProximitySensor;
+    public float value;
 
-	private ProximitySensor(Context context) {
-		Log.d(TAG, "constructor");
+    private ProximitySensor(Context context) {
+        Log.d(TAG, "constructor");
 
-		SensorManager mSensorManager = (SensorManager) context.getSystemService(Service.SENSOR_SERVICE);
-		try{
+        SensorManager mSensorManager = (SensorManager) context.getSystemService(Service.SENSOR_SERVICE);
+        try {
             Sensor sensor = mSensorManager.getSensorList(Sensor.TYPE_PROXIMITY).get(0);
             SensorEventListener listener = new MySensorEventListener();
             mSensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_FASTEST);
-        }catch (IndexOutOfBoundsException e){
-            Log.
+        } catch (IndexOutOfBoundsException e) {
+            Log.e(TAG, e.getMessage(), e);
+            Toast.makeText(context, context.getString(R.string.toast_no_proximity_sensor_found), Toast.LENGTH_LONG).show();
         }
     }
 
-	public static ProximitySensor getInstance(Context context) {
-		if (mProximitySensor == null) {
-			mProximitySensor = new ProximitySensor(context);
-		}
-		return mProximitySensor;
-	}
-	
-	public static boolean isFar(Context context){
-		return getInstance(context).value != 0;
-	}
+    public static ProximitySensor getInstance(Context context) {
+        if (mProximitySensor == null) {
+            mProximitySensor = new ProximitySensor(context);
+        }
+        return mProximitySensor;
+    }
 
-	class MySensorEventListener implements SensorEventListener {
-		@Override
-		public void onSensorChanged(SensorEvent event) {
-			float value = event.values[0];
-			Log.d(TAG, "Proximity value=" + value);
-			ProximitySensor.this.value = value;
-		}
+    public static boolean isFar(Context context) {
+        return getInstance(context).value != 0;
+    }
 
-		@Override
-		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			// TODO Auto-generated method stub
+    class MySensorEventListener implements SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float value = event.values[0];
+            Log.d(TAG, "Proximity value=" + value);
+            ProximitySensor.this.value = value;
+        }
 
-		}
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
 
-	}
+        }
+
+    }
 }
